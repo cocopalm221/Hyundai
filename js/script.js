3aa92836656b8d3dd62121211e142b29e0e65b7c
h.young <cocotree221@gmail.com>
2023-03-15 19:27:44 +0900

    1차버전

--- js/script.js
+++ js/script.js
@@ -1,9 +1,55 @@
 window.onload = function () {
+  AOS.init();
+
+  // ** FADE OUT FUNCTION **
+  // fadeOut(element : document.querySelector(대상))
+  function fadeOut(el) {
+    //대상.style.투명도 = 불투명
+    el.style.opacity = 1;
+
+    (function fade() {
+      //대상.style.투명도 -= 0.1 감소
+      if ((el.style.opacity -= 0.1) < 0) {
+        el.style.display = "none";
+      } else {
+        //웹브라우저 프레임갱신
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
+
+  // ** FADE IN FUNCTION **
+  // fadeIn(element : document.querySelector(대상))
+  function fadeIn(el, display) {
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    (function fade() {
+      var val = parseFloat(el.style.opacity);
+      if (!((val += 0.1) > 1)) {
+        el.style.opacity = val;
+        requestAnimationFrame(fade);
+      }
+    })();
+  }
   //modal
   let body = document.querySelector("body");
   let modal = document.querySelector(".modal");
   modal.addEventListener("click", function () {
-    modal.style.display = "none";
+    // modal.style.display = "none";
+    // body.classList.add("active");
+    // fadeOut(modal);
+    anime({
+      targets: ".modal",
+      delay: 200,
+      duration: 500,
+      opacity: 0,
+      easing: "linear",
+      complete: function () {
+        // alert("gogo");
+        modal.style.display = "none";
+        body.classList.add("acctive");
+      },
+    });
     body.classList.add("active");
   });
 
